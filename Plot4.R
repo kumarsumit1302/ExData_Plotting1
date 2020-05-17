aab77b6368e57b1b08d9c8df9b4ffdf74f329b45
kumarsumit1302 <kumarsumit1302@gmail.com>
2020-05-17 17:44:25 +0530

    Code for Plot 4

---
+++ Plot4.R
@@ -0,0 +1,22 @@
+consumption_data <- read.csv("household_power_consumption.txt",header = T,sep = ";",colClasses = "character")
+feb1_and_feb2_data <- consumption_data[consumption_data$Date %in% c("1/2/2007","2/2/2007"),]
+
+feb1_and_feb2_data$date_time <- strptime(paste(feb1_and_feb2_data$Date,feb1_and_feb2_data$Time),format = "%d/%m/%Y %H:%M:%S")
+
+png(filename = "plot4.png",width = 480,height=480)
+
+par(mfrow=c(2,2))
+
+plot(feb1_and_feb2_data$date_time,as.numeric(feb1_and_feb2_data$Global_active_power),type = "l",xlab = "datetime",ylab ="Global active power (Kilowatts)")
+
+plot(feb1_and_feb2_data$date_time,as.numeric(feb1_and_feb2_data$Voltage),type = "l",xlab = "datetime",ylab ="Voltage")
+
+
+with(feb1_and_feb2_data,plot(date_time,as.numeric(feb1_and_feb2_data$Sub_metering_1),type="l",col="black",xlab="datetime",ylab="Energy sub metering"))
+with(feb1_and_feb2_data,lines(date_time,as.numeric(feb1_and_feb2_data$Sub_metering_2),type="l",col="red"))
+with(feb1_and_feb2_data,lines(date_time,as.numeric(feb1_and_feb2_data$Sub_metering_3),type="l",col="blue"))
+legend("topright",pch = "-",col = c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=0.75)
+
+plot(feb1_and_feb2_data$date_time,as.numeric(feb1_and_feb2_data$Global_reactive_power),type = "l",xlab = "datetime",ylab ="Global reactive power")
+
+dev.off()
